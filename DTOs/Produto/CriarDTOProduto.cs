974808c29eb03f3ed4ab11f7278f3c1472ed6d79
victorbiguete <victorbiguete@gmail.com>
2025-04-11 13:53:56 -0300

    Criação do DTO Produto e Spans na view Cadastrar para validação dos erros

---
+++ DTOs/Produto/CriarDTOProduto.cs
@@ -0,0 +1,31 @@
+﻿using LojaProdutos.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaProdutos.DTOs.Produto
+{
+    public class CriarDTOProduto
+    {
+        [Required(ErrorMessage ="Digite um Nome")]
+        
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage ="Digite a Marca")]
+        public string Marca { get; set; } = string.Empty;
+
+        [Required(ErrorMessage ="Insira uma Foto")]
+        public string Foto { get; set; } = string.Empty;
+
+        [Required(ErrorMessage ="Digite um Valor")]
+        [MinLength(1)]
+        public double Valor { get; set; } = 0;
+
+        [Required(ErrorMessage ="Digite uma Quantidade")]
+        [MinLength(1)]
+        public int QuantidadeEstoque { get; set; } = 0;
+
+        [Required(ErrorMessage ="Selecione uma Categoria")]
+        public int CategoriaModelId { get; set; }
+        
+    }
+}
