b87af480c188062d5d414dff1408108bb8c92ff7
victorbiguete <victorbiguete@gmail.com>
2025-04-14 17:49:47 -0300

    Criado Relatórios de vendas xml

--- Controllers/EstoqueController.cs
+++ Controllers/EstoqueController.cs
@@ -1,5 +1,7 @@
-﻿using LojaProdutos.Services.Estoque;
+﻿using ClosedXML.Excel;
+using LojaProdutos.Services.Estoque;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace LojaProdutos.Controllers
 {
@@ -23,6 +25,43 @@ namespace LojaProdutos.Controllers
             var produtoBaixado = await _estoqueInterface.CriarRegistro(id);
             TempData["MensagemSucesso"] = "Compra Realizada com Sucesso";
             return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult GerarRelatorio()
+        {
+            var dados = BuscarDados();
+
+            using (XLWorkbook workbook = new XLWorkbook())
+            {
+                workbook.AddWorksheet(dados, "Dados Vendas");
+
+                using(MemoryStream ms  = new MemoryStream())
+                {
+                    workbook.SaveAs(ms);
+                    return File(ms.ToArray(), "application/vmd.openxmlformats-officedocument.spredsheetml.sheet", "Vendas.xls");
+                }
+            }
+        }
+        private DataTable BuscarDados()
+        {
+            DataTable dataTable = new DataTable();
+
+            dataTable.TableName = "Dados Vendas - Produtos";
+            dataTable.Columns.Add("ProdutoID", typeof(int));
+            dataTable.Columns.Add("Categoria", typeof(string));
+            dataTable.Columns.Add("Data da Compra", typeof(DateTime));
+            dataTable.Columns.Add("Valor Total", typeof(double));
+
+            var dados = _estoqueInterface.ListagemRegistro();
+
+            if(dados.Count > 0)
+            {
+                foreach (var registro in dados)
+                {
+                    dataTable.Rows.Add(registro.ProdutoId,registro.CategoriaNome,registro.DataCompra,registro.Total);
+                }
+            }
+            return dataTable;
         }
     }
 }
